640b7f43e4027483569ba300a4e5264093f22a27
unknown <frimpongi50@gmail.com>
2020-10-21 20:27:15 +0000

    created the user interface for the application

---
+++ main.js
@@ -0,0 +1,7 @@
+let customer = document.getElementById('customerName');
+let tipPercent = document.getElementById('percentage');
+let amount = document.getElementById('amount');
+
+document.getElementById('calculate').addEventListener('click', function() {
+    alert("Hello " + customer.value)
+})
