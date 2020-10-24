9ae8dbd7350dcb63d93ca17214a16190abc6ea74
unknown <frimpongi50@gmail.com>
2020-10-24 19:50:40 +0000

    added codes to display results

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 let customer = document.getElementById('customerName');
-let tipPercent = document.getElementById('percentage');
+let tipPercent = document.getElementById('percent');
 let amount = document.getElementById('amount');
 
+let tipAmount;
+
 document.getElementById('calculate').addEventListener('click', function() {
-    alert("Hello " + customer.value)
+    document.getElementById('displayCustomerName').innerText = customer.value;
+    document.getElementById('displayTipPercentage').innerText = tipPercent.value;
+    document.getElementById('displayAmount').innerText = amount.value;
+    
+    tipAmount = (tipPercent.value / 100) * amount.value;
+    
+    document.getElementById('displayTipAmount').innerText = tipAmount;
 })
